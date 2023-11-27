e30acabfecfdff7779b916a96c7cb139c1800948
PAU GARCIA PASCUAL <114986355+Paunetum@users.noreply.github.com>
2023-11-27 17:18:59 +0100

    second commit

--- back/api.js
+++ back/api.js
@@ -1,6 +1,18 @@
 import express, { json } from 'express'
+import { corsMiddleware } from './middlewares/cors.js'
+import { createTasksRouter } from './routes/tasks.js'
 
 const app = express()
 
 app.use(json())
+app.use(corsMiddleware())
+app.disable('x-powered-by')
 
+app.use('/tasks', createTasksRouter())
+
+const PORT = process.env.PORT ?? 3000
+
+app.listen(PORT, () => {
+  console.log(`App listenint on http://localhost:${PORT}`)
+  console.log(process.env)
+})
---
+++ back/routes/tasks.js
@@ -0,0 +1,7 @@
+import { Router } from 'express'
+
+export const createTasksRouter = () => {
+  const tasksRouter = Router()
+
+  tasksRouter.get('/')
+}
